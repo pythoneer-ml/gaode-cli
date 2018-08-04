d18fc2e2b20be9bf0af957cf39f71c94b18b05bb
Albin Zeng <binwei.zbw@alibaba-inc.com>
2018-08-04 15:12:20 +0800

    chore: typo.

--- setup.py
+++ setup.py
@@ -14,32 +14,32 @@ setup(
     author_email='binwei.zeng3@gmail.com',
     maintainer='albin3',
     maintainer_email='binwei.zeng3@gmail.com',
-    license='BSD License',
+    license='MIT License',
     packages=find_packages(),
     platforms=["all"],
     url='https://github.com/pythoneer-ml/pypi-gaode-api',
     include_package_data=True,
     install_requires=[
-        'fire>=0.1.3',
-        'urllib3>=1.22'
+      'fire>=0.1.3',
+      'urllib3>=1.22'
     ],
     scripts=[
       'bin/gaode-api-intersection',
       'bin/gaode-api-config'
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries'
+      'Development Status :: 4 - Beta',
+      'Operating System :: OS Independent',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: Implementation',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Topic :: Software Development :: Libraries'
     ],
 )
